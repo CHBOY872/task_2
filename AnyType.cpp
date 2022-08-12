23c0e7d18fd775312031fa78ec75a21a9436098b
CH <ch@CHs-MacBook-Pro.local>
2022-08-12 22:26:08 +0200

    reserv -2

--- AnyType.cpp
+++ AnyType.cpp
@@ -2,25 +2,38 @@
 
 #include "AnyType.hpp"
 
+class UnTypedMPunsafe
+{
+    friend class UnTypedMP;
+    friend class AnyType;
+    void *var;
+    int size;
+
+public:
+    UnTypedMPunsafe(int _size);
+};
+
 UnTypedMPunsafe::UnTypedMPunsafe(int _size)
     : var(malloc(_size)), size(_size) {}
 
 ////////////////////////////
 
 UnTypedMP::UnTypedMP(int _size)
-    : UnTypedMPunsafe(_size) {}
-UnTypedMP::~UnTypedMP() { free(var); }
+    : u(_size) {}
+void *UnTypedMP::GetVar() { return u.var; }
+int &UnTypedMP::GetSize() { return u.size; }
+UnTypedMP::~UnTypedMP() { free(u.var); }
 UnTypedMP &UnTypedMP::operator=(const UnTypedMP &a)
 {
     if (this != &a)
     {
-        free(var);
-        var = malloc(a.size);
-        size = a.size;
+        free(GetVar());
+        u.var = malloc(a.u.size);
+        u.size = a.u.size;
         int i;
-        unsigned char *buf_from = (unsigned char *)a.var;
-        unsigned char *buf_to = (unsigned char *)var;
-        for (i = 0; i < size; i++)
+        unsigned char *buf_from = (unsigned char *)a.u.var;
+        unsigned char *buf_to = (unsigned char *)u.var;
+        for (i = 0; i < u.size; i++)
             buf_to[i] = buf_from[i];
     }
     return *this;
@@ -31,7 +44,7 @@ UnTypedMP &UnTypedMP::operator=(const UnTypedMP &a)
 AnyType::AnyType() : ptr(new UnTypedMP(sizeof(void *))) {}
 AnyType::AnyType(const AnyType &a)
 {
-    ptr = new UnTypedMP(a.ptr->size);
+    ptr = new UnTypedMP(a.ptr->u.size);
 }
 AnyType::~AnyType() { delete ptr; }
 
--- AnyType.hpp
+++ AnyType.hpp
@@ -2,25 +2,20 @@
 #define ANYTYPE_HPP_SENTRY
 
 class AnyType;
-class UnTypedMPunsafe
-{
-protected:
-    friend class AnyType;
-    void *var;
-    int size;
-
-public:
-    UnTypedMPunsafe(int _size);
-};
-
-class UnTypedMP : private UnTypedMPunsafe
+class UnTypedMPunsafe;
+class UnTypedMP
 {
     friend class AnyType;
+    UnTypedMPunsafe u;
 
 public:
     UnTypedMP(int _size = 0);
     ~UnTypedMP();
     UnTypedMP &operator=(const UnTypedMP &a);
+
+private:
+    void *GetVar();
+    int &GetSize();
 };
 
 class AnyType
@@ -32,7 +27,7 @@ public:
     template <class T>
     AnyType(T a) : ptr(new UnTypedMP(sizeof(T)))
     {
-        *(T *)ptr->var = a;
+        *(T *)ptr->GetVar() = a;
     }
     AnyType(const AnyType &a);
     ~AnyType();
