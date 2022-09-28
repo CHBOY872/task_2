07cc0af7d6f6ace1b15082e09189581944df4820
Yurii <yuriy dot chalov03 at gmail dot com>
2022-09-28 16:36:30 +0200

    Add a test

--- src/test.cpp
+++ src/test.cpp
@@ -26,6 +26,42 @@ TEST(MainTest, ThrowExceptionTest)
         }
     },
                  ExceptionType);
+    EXPECT_THROW({
+        try
+        {
+            AnyType x = 'A';
+            int z = x.ToInt();
+            printf("%d\n", z);
+        }
+        catch (ExceptionType &a)
+        {
+            fprintf(stderr, "%s\n", a.GetMsg());
+            throw;
+        }
+        catch (...)
+        {
+            FAIL();
+        }
+    },
+                 ExceptionType);
+    EXPECT_THROW({
+        try
+        {
+            AnyType x = 2.6f;
+            double z = x.ToDouble();
+            printf("%lf\n", z);
+        }
+        catch (ExceptionType &a)
+        {
+            fprintf(stderr, "%s\n", a.GetMsg());
+            throw;
+        }
+        catch (...)
+        {
+            FAIL();
+        }
+    },
+                 ExceptionType);
 }
 
 TEST(MainTest, NoThrowExceptionTest)
@@ -47,10 +83,28 @@ TEST(MainTest, NoThrowExceptionTest)
             FAIL();
         }
     });
+    EXPECT_NO_THROW({
+        try
+        {
+            AnyType x = 2.5;
+            x = 'x';
+            char y = x.ToChar();
+            printf("%c\n", y);
+        }
+        catch (ExceptionType &a)
+        {
+            fprintf(stderr, "%s\n", a.GetMsg());
+            throw;
+        }
+        catch (...)
+        {
+            FAIL();
+        }
+    });
 }
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
